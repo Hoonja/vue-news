fa0c83916237741846f0e9d977c88d943c77bc64
JAE HOON CHOI <hoonja@gmail.com>
2019-01-30 22:54:27 +0900

    styling the item page

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        {{item}}
+        <section>
+            <div class="user-container">
+                <div>
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="user-description">
+                    <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+                    <div class="time">
+                        {{ item.time_ago }}
+                    </div>
+                </div>                
+            </div>
+            <h2>{{ item.title }}</h2>
+        </section>
+        <section>
+            <div v-html="item.content"></div>
+        </section>
     </div>    
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
     computed: {
-        item() {
-            return this.$store.state.item
-        }
+        // item() {
+        //     return this.$store.state.item
+        // }
+        ...mapGetters({
+            item: 'fetchedItem'
+        })
     },
     created() {
         const itemId = this.$route.params.id
@@ -18,6 +39,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+}
 
+.fa-user {
+    font-size: 2rem;
+}
+
+.user-description {
+    padding-left: 8px;
+}
+
+.time {
+    font-size: 0.7rem;
+}
 </style>
